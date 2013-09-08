056297dfc6e027658b437d6d2ef698c69d01b12d
sdai13 <simon_dai13@yahoo.com>
2013-09-08 18:28:58 -0400

    basis for file classes

--- DSProject1/src/TransactionalFileInputStream.java
+++ DSProject1/src/TransactionalFileInputStream.java
@@ -1,18 +1,42 @@
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.Serializable;
+import java.io.*;
 
 
 public class TransactionalFileInputStream extends InputStream implements
 		Serializable {
 
-	public TransactionalFileInputStream(String filename)
-	{}
+	private String filename;
+	private long fileLocation;
+	Object lock;
+
+	public TransactionalFileInputStream(String filename, Object lock) {
+		
+		this.filename = filename;
+		fileLocation = 0;
+		this.lock = lock;
+	}
+
+	public TransactionalFileInputStream(String filename) {
+		
+		this.filename = filename;
+		fileLocation = 0;
+	}
 	
 	@Override
 	public int read() throws IOException {
-		// TODO Auto-generated method stub
-		return 0;
+
+		synchronized(lock){
+		
+			File fileToRead = new File(filename);
+			RandomAccessFile stream = new RandomAccessFile(fileToRead, "r");
+			stream.seek(fileLocation);
+			
+			int toReturn = stream.read();
+			
+			stream.close();
+			fileLocation++;
+			
+			return toReturn;
+		}
 	}
 
 }
--- DSProject1/src/TransactionalFileOutputStream.java
+++ DSProject1/src/TransactionalFileOutputStream.java
@@ -1,20 +1,43 @@
+import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.RandomAccessFile;
 import java.io.Serializable;
 
 
 public class TransactionalFileOutputStream extends OutputStream implements
 		Serializable {
 
+	private String filename;
+	private long fileLocation;
+	Object lock;
 
-	public TransactionalFileOutputStream(String filename, boolean someBool)
-	{}
+	public TransactionalFileOutputStream(String filename, boolean someBool){
+		
+	}
+
+	public TransactionalFileOutputStream(String filename, Object lock){
+		
+		this.filename = filename;
+		this.fileLocation = 0;
+		this.lock = lock;
+	}
 	
 	
 	@Override
 	public void write(int b) throws IOException {
-		// TODO Auto-generated method stub
-
+		
+		synchronized(lock){
+			
+			File fileToRead = new File(filename);
+			RandomAccessFile stream = new RandomAccessFile(fileToRead, "w");
+			stream.seek(fileLocation);
+			
+			stream.write(b);
+			
+			stream.close();
+			fileLocation++;
+		}
 	}
 
 }
