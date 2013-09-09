8559d370a93937ea4bfacdda2b85eecf00ea6c9e
mo123455 <bobjones600@gmail.com>
2013-09-09 00:53:39 -0400

    Outlined process manager class, this is where most work will be done.

--- DSProject1/src/ProcessManager.java
+++ DSProject1/src/ProcessManager.java
@@ -1,4 +1,31 @@
+import java.util.TreeMap;
+
 
 public class ProcessManager {
-
+	//Structure to hold info on running process
+	private class ProcessInfo{
+		String url;
+		int portNum;
+		long processID;
+		boolean isRunning;
+		
+		public ProcessInfo(String url, int portNum, long processID){
+			isRunning=true;
+			this.url=url;
+			this.processID=processID;
+			this.portNum=portNum;
+		}
+		
+	}
+	
+	//List of processes maped to their ID's
+	TreeMap processTable;
+	
+	public ProcessManager(){
+		processTable = new TreeMap();
+	}
+	
+	public long RunProcess(MigratableProcess p){return 0;}
+	public boolean MigrateProcess(String destination){return true;}
+	public boolean TerminateProcess(){return true;}
 }
