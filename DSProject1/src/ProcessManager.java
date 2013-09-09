082655af206a0e7fb9320c501e1a6d44084a364b
mo123455 <bobjones600@gmail.com>
2013-09-09 19:23:29 -0400

    Added some code for port selection

--- DSProject1/src/ProcessManager.java
+++ DSProject1/src/ProcessManager.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 public class ProcessManager {
 
+	public static int PORT_NUM = 5555;
 	private List processes;
 	
 	public ProcessManager(){
@@ -11,7 +12,7 @@ public class ProcessManager {
 		
 	}
 	
-	public Object startProcess(String className, String[] args){
+	public MigratableProcess startProcess(String className, String[] args){
 		
 		Object instance = null;
 		try {
@@ -41,18 +42,43 @@ public class ProcessManager {
 			e.printStackTrace();
 		}
 		
+		MigratableProcess newProcess=null;
 		if(!(instance instanceof MigratableProcess)){
 			
 			//Handle
 		}
+		else {
+			newProcess=(MigratableProcess)instance;
+		}
 			
+		//Start executing the process
+		Thread processThread = new Thread(newProcess);
+		processThread.run();
 		
-		return instance;
+		return newProcess;
 	}
 	
 	public void migrateProcess(){
 		
 	
+	}
+	
+	//The ability to specify a command-line port is added in the event 
+	//that the default port PORT_NUM is unavailable.  However, it is the 
+	//responsibility of the user to ensure that the port number is consistent
+	//across nodes
+	public static void main(String [] args)
+	{
+		int port=0;
+		if(args.length > 0){
+			try{
+				port = Integer.parseInt(args[0]);
+			}catch (NumberFormatException e) {
+				System.out.println("Error, invalid port argument, using default: " + Integer.toString(PORT_NUM));
+				port = PORT_NUM;
+			}
+		}
+		PORT_NUM = port;
 	}
 	
 }
