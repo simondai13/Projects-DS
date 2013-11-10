8da7b572fce3de86b43c2916b30530b41fa8dc92
sdai13 <simon_dai13@yahoo.com>
2013-11-10 17:35:50 -0500

    basic FileSystem API

--- DSProject3/src/ComputeNode.java
+++ DSProject3/src/ComputeNode.java
@@ -1,10 +1,63 @@
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.List;
 
-
-public class ComputeNode {
+public class ComputeNode implements Runnable{
 
 	
 	//files on this compute node
-	List<File> localFiles;
+	private FileSystemNode fileSystem;
+	private ServerSocket server;
+	
+	
+	public ComputeNode(int portnum) throws IOException{
+		
+		
+		server = new ServerSocket(portnum);
+	}
+
+
+	private class ConnectionHandle implements Runnable {
+		private Socket client;
+		
+		public ConnectionHandle(Socket client){
+			this.client=client;
+		}
+		
+		@Override
+		public void run() {
+			try{ 
+				 InputStream in = client.getInputStream();
+
+			 } catch (IOException e) {
+			 }
+		}
+	}
+		
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+		while(true) {
+			try {
+				Socket client = server.accept();
+				
+				//Generate a connection handle and run it in a 
+				//separate thread
+				ConnectionHandle ch = new ConnectionHandle(client);
+				Thread t = new Thread(ch);
+				t.start();
+				
+			} catch (IOException e) {
+				
+				System.out.println("CONNECTION FAILURE");
+			}
+			 
+		}
+	}
 }
