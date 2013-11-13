14606ba15b48e3461f6983db2aa9f03e08545888
mo123455 <bobjones600@gmail.com>
2013-11-13 17:54:37 -0500

    Changes to master

--- DSProject3/src/FileLocationRequest.java
+++ DSProject3/src/FileLocationRequest.java
@@ -1,15 +1,7 @@
 import java.io.Serializable;
-import java.net.InetSocketAddress;
 
 
 public class FileLocationRequest implements Serializable {
 	private static final long serialVersionUID = 1L;
 	public String filename;
-	
-	public enum Type {
-		ADD, LOC
-	}
-	Type type;
-	
-	public InetSocketAddress[] nodes;
 }
