2bad4b8d15ad1737ec33e66721d3b293030b068e
sdai13 <simon_dai13@yahoo.com>
2013-11-17 19:44:30 -0500

    wrote histogram

--- DSProject3/src/Histogram.java
+++ DSProject3/src/Histogram.java
@@ -11,7 +11,7 @@ public class Histogram implements MapReducer {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		return line;
+		return line + " 1";
 	}
 
 	@Override
@@ -21,6 +21,22 @@ public class Histogram implements MapReducer {
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		}
+		
+		String[] record = line.split(" ");
+		String word = record[0];
+		int count = Integer.parseInt(record[1]);
+		for(int i = 0; i < lines.size() ; i++){
+
+			String[] recordData = lines.get(i).split(" ");
+			String w = recordData[0];
+			int num = Integer.parseInt(recordData[1]);
+			int total = num+count;
+			if(w.equals(word)){
+				lines.set(i, w+" "+total);
+				return lines;
+			}
+			
 		}
 		lines.add(line);
 		return lines;
